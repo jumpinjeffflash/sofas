6a3a30160c66645fdb2cb90b7ffa33ffe1e3106d
jumpinjeffflash <jeffburslem@gmail.com>
2024-03-18 15:51:58 -0500

    Update sofas.py
    commented out caching per the recommendation of claude.ai

--- sofas.py
+++ sofas.py
@@ -33,7 +33,7 @@ with st.expander("Click here for more details about how this model was built"):
         st.write("""Each image also comes with 3x labels, which correspond to the 3x dimensions: width, depth and height. They were scored with a 1 if the dimension was bigger than a standard sofa and 0 if it was smaller. The model uses that information along with the patterns it learned to calculate the probability of whether a new image is bigger/smaller than a standard 3-seater.""")
         st.write("""Over 1,000 images were used to train and test the model. That's A LOT of photo shoots!""")
             
-@st.cache(persist=True)
+# @st.cache(persist=True)
 
 def import_and_predict(image_data, model):
         size = (224,224)
